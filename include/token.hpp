148bfa6e4f8e0f2068433c6e6e479bceabce2581
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-06 00:26:13 +0900

    Add class IdentTok

--- include/token.hpp
+++ include/token.hpp
@@ -1,6 +1,8 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 
+#include <string>
+
 enum class TokenKind {
   Identifier,
   Keyword,
@@ -22,5 +24,21 @@ class Token {
   private:
   const TokenKind kind;
 };
+
+class IdentTok : public Token {
+  public:
+  IdentTok(const std::string &i)
+      : Token(TokenKind::Identifier), ident(i) {
+  }
+  ~IdentTok() override = default;
+  static bool
+  classof(const Token *T) {
+    return T->gteKind() == TokenKind::Identifier;
+  }
+
+  private:
+  std::string ident;
+};
+
 
 #endif /*! TOKEN_HPP */

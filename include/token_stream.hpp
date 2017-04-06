7c2d559b661ce3b941d91f2fcc35adc61486e9c9
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-06 11:51:43 +0900

    Add class TokenStream

--- include/token_stream.hpp
+++ include/token_stream.hpp
@@ -2,5 +2,22 @@
 #define TOKEN_STREAM_HPP
 
 #include "token.hpp"
+#include <vector>
+
+class TokenStream {
+  public:
+  TokenStream() : cur_index(0), tokens() {
+  }
+  ~TokenStream() = default;
+  bool ungetToken(size_t times = 1);
+  bool improve();
+  bool pushToken(const TokenRef &);
+  TokenRef getToken() const;
+  TokenKind getTokenKind() const;
+
+  private:
+  size_t cur_index;
+  std::vector<TokenRef> tokens;
+};
 
 #endif /*! TOKEN_STREAM_HPP */

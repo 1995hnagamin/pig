bfafc678314c368077a9432772c3d45a9d92e6d7
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-08 23:48:48 +0900

    Add helper function show_help to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include "lexer.hpp"
 #include <iostream>
 
+namespace {
+
+void
+show_help(const std::string &program) {
+  std::cerr << "usage: " << program << " <filename>" << std::endl;
+}
+
+} // namespace
+
 int
 main(int argc, char **argv) {
   const auto tokens = lex_from_file(argv[1]);

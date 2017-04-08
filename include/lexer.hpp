36c9536bd709e84942aef6adbc1c7afbaf325ffa
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-08 23:47:28 +0900

    Add lex_from_file to lexer.hpp

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -3,5 +3,9 @@
 
 #include "token.hpp"
 #include "token_stream.hpp"
+#include <memory>
+#include <string>
+
+std::unique_ptr<TokenStream> lex_from_file(const std::string &);
 
 #endif /*! LEXER_HPP */

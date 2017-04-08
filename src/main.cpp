5c59ea5451548b18c57e5d97dd8fb26919a2af94
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-08 23:50:41 +0900

    Show help if no command line argument is passed

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "lexer.hpp"
+#include <cassert>
 #include <iostream>
 
 namespace {
@@ -12,6 +13,11 @@ show_help(const std::string &program) {
 
 int
 main(int argc, char **argv) {
+  if (argc < 2) {
+    assert(argc == 1);
+    show_help(argv[0]);
+    return 0;
+  }
   const auto tokens = lex_from_file(argv[1]);
   return 0;
 }
